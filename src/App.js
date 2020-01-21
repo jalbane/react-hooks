848adde09027c74f9ed7207ab8cf0bc8ee1d9bdf
Jeff Albanese <jalbane9@gmail.com>
2020-01-20 22:59:48 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Display from './Display';
 
 function App() {
+  const [todoText, setTodoText] = useState([
+  ]);
+
+  const addTodo = text => {
+    const completed = false
+    const newTodos = [...todoText, {text, completed}];
+    setTodoText(newTodos);
+  }
+
+  const completeTask = index => {
+    const copy = todoText.slice()
+    const completedItem = (copy[index].completed)
+    copy.splice(index, 1, {text: copy[index].text, completed: !completedItem} )
+    //const newTodos = [...copy];
+    setTodoText(copy)
+  }
+
+  const deleteTask = index => {
+    if (todoText[index].completed){
+      const copy = todoText.slice()
+      copy.splice(index, 1)
+      setTodoText(copy)
+    }
+  }
+
+  function AddTodo({addTodo}){
+    const [value, setValue] = useState([{
+      text: ' ',
+      completed: false
+    }]);
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      if(!value) return;
+      addTodo(value);
+      setValue('');
+    }
+
+    return(
+      <form onSubmit = {handleSubmit}>
+        <input style= {{width: 390}} placeholder = 'Add a Todo...' value = {value.text} onChange = { (e) =>setValue([e.target.value])} />
+      </form>
+      );
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  <div className = 'app'>
+    <div className = 'todo-list'>
+      { todoText.map( (index, i) => <Display text = {index.text} completed = {index.completed} index = {i} key = {index.text} handleComplete = {completeTask} handleDelete = {deleteTask}/> )}
+      
+      <AddTodo addTodo={addTodo}/>
     </div>
+  </div>
   );
 }
 
---
+++ src/Display.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import './display.css'
+
+export default function Display(props){
+	let [date, setDate] = useState(Date())
+
+	const displayDate = () => {
+		date = date.split('GMT');
+		return date[0];
+	}
+
+	return (
+		<div style = {{ 
+						backgroundColor: '#fff',
+						fontSize: 12
+					 }}>
+			{ props.completed 
+			 ? <span><button onClick = {() => props.handleDelete(props.index)}> Delete </button></span>
+			 : <span><button onClick = {() => props.handleComplete(props.index)}> Complete </button></span>
+			}
+			<div className = 'todo-text'> 
+				{displayDate()}
+				<br></br>
+				{props.text}  
+			</div>
+		</div>
+	);
+}
